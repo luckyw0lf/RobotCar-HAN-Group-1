46ec9ec390174de77ff8c99e04e12b2e79b0662b
Luckyw0lf <ruben@balduk.nl>
2025-10-21 14:29:59 +0200

    Motor logic

--- include/motor.h
+++ include/motor.h
@@ -3,19 +3,15 @@
 
 // definitions of functions for forward driving on the motors
 void leftMotor(int speed);
-void leftDownMotor(int speed);
-void RightMotor(int speed);
-void RightDownMotor(int speed);
-//definitions of functions for backwards driving on the motors
-void leftMotorRev(int speed);
-void leftDownMotorRev(int speed);
-void RightMotorRev(int speed);
-void RightDownMotorRev(int speed);
+void rightMotor(int speed);
 
 //movement for the car 
 void turnLeft(int speed);
 void turnRight(int speed);
 void backwards(int speed);
 void forward(int speed);
+
+// emergency stop
+void emergencyStop();
 
 #endif
---
+++ src/motor.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <pins.h>
+#include <motor.h>
+
+
+void leftMotor(int speed){
+    if(speed < 0){
+        digitalWrite(MOTOR_LEFT_P, LOW);
+        analogWrite(MOTOR_LEFT_N, speed);
+        return;
+    }
+    analogWrite(MOTOR_LEFT_P, speed);
+    digitalWrite(MOTOR_LEFT_N, LOW);
+}
+
+void rightMotor(int speed){
+    if(speed < 0){
+        digitalWrite(MOTOR_RIGHT_P, LOW);
+        analogWrite(MOTOR_RIGHT_N, speed);
+
+        return;
+    }
+    analogWrite(MOTOR_RIGHT_P, speed);
+    digitalWrite(MOTOR_RIGHT_N, LOW);
+}
+
+//turn the car right
+void turnRight(int speed) {
+    leftMotor(speed);
+    rightMotor(-speed);
+}
+//turn the car left 
+void turnLeft(int speed) {
+    leftMotor(-speed);
+    rightMotor(speed);
+}
+//forward car movement
+void forward(int speed){
+    leftMotor(speed);
+    rightMotor(speed);
+}
+//backwards car movement 
+void backwards(int speed){
+    leftMotor(-speed);
+    rightMotor(-speed);
+}
+// stops all motors at the same time 
+void emergencyStop() {
+    leftMotor(0);
+    rightMotor(0);
+}
