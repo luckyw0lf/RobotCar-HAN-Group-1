ef0809468af2ba3bca9cb655a77e736e11500d2f
Luckyw0lf <ruben@balduk.nl>
2025-10-21 15:25:45 +0200

    Fixed the fuckups

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,30 @@
 #include <motor.h>
 
 void setup() {
-  pinMode(MOTOR_LEFT_F, OUTPUT);
-  pinMode(MOTOR_LEFT_B, OUTPUT);
-  pinMode(MOTOR_RIGHT_F, OUTPUT);
-  pinMode(MOTOR_RIGHT_B, OUTPUT);
-  pinMode(MOTOR_DOWN_LEFT_F, OUTPUT);
-  pinMode(MOTOR_DOWN_LEFT_B, OUTPUT);
-  pinMode(MOTOR_DOWN_RIGHT_F, OUTPUT);
-  pinMode(MOTOR_DOWN_RIGHT_B, OUTPUT);
+  pinMode(MOTOR_LEFT_P, OUTPUT);
+  pinMode(MOTOR_LEFT_N, OUTPUT);
+  pinMode(MOTOR_RIGHT_P, OUTPUT);
+  pinMode(MOTOR_RIGHT_N, OUTPUT);
 
   pinMode(LED_LEFT_OUTSIDE, INPUT);
   pinMode(LED_LEFT_INSIDE, INPUT);
   pinMode(LED_RIGHT_INSIDE, INPUT);
   pinMode(LED_RIGHT_OUTSIDE, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  turnRight(150); // 0-255 max 
-  delay(1000);
-  emergencyStop(); // stops all motors at the same time 
-  delay(1000);
-
-  turnLeft(150);
+  turnRight(90); // 0-255 max 
   delay(1000);
   emergencyStop();
+  delay(1000);
+  turnLeft(90);
+  delay(1000);
+  emergencyStop();
+  delay(1000);
+  forward(90);
   delay(2000);
+  emergencyStop();
+  delay(5000);
 }
