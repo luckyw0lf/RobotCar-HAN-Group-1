cbe3ab125105003d378fb42437b8fa6b3d1e0e4a
Luckyw0lf <ruben@balduk.nl>
2025-10-29 12:26:10 +0100

    Bluetooth and basic motor control

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -4,127 +4,146 @@
 #include "bluetooth.h"
 
 // Motor speed settings - will be updated from app
-static int currentSpeed = 200;  // Default speed (0-255)
-static int turnSpeed = 180;
+static int currentSpeed = 100;  // Default speed (0-255)
+static int turnSpeed = 130;
 static int diagonalSpeed = 170;
 
 // Command buffer
 static char commandBuffer[10];
 static int bufferIndex = 0;
 
-void bluetoothInit(void) {
+void  bluetoothInit(void) {
+    // Serial.begin(38400);
     Serial.begin(9600);
 }
 
-// Function to check if a string is a number
-int isNumber(char* str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        if (str[i] < '0' || str[i] > '9') {
-            return 0;  // Not a number
-        }
-        i++;
-    }
-    return 1;  // Is a number
-}
+// Temp bluetooth logic
+void bluetoothUpdate(){
+ if (Serial.available()) {
+    char cmd = Serial.read();
 
-// Function to convert string to integer
-int stringToInt(char* str) {
-    int result = 0;
-    int i = 0;
-    while (str[i] != '\0') {
-        result = result * 10 + (str[i] - '0');
-        i++;
+    switch (cmd) {
+      case 'F': forward(currentSpeed); break;
+      case 'B': forward(-currentSpeed); break;
+      case 'L': turnLeft(turnSpeed);    break;
+      case 'R': turnRight(turnSpeed);   break;
+      case 'S': emergencyStop();   break;
+      default:  /* ignore */      break;
     }
-    return result;
+  }
+  
 }
+// // Function to check if a string is a number
+// int isNumber(char* str) {
+//     int i = 0;
+//     while (str[i] != '\0') {
+//         if (str[i] < '0' || str[i] > '9') {
+//             return 0;  // Not a number
+//         }
+//         i++;
+//     }
+//     return 1;  // Is a number
+// }
 
-void processCommand(char* command) {
-    if (isNumber(command)) {
-        int speedLevel = stringToInt(command);
-        if (speedLevel >= 0 && speedLevel <= 10) {
-            currentSpeed = (speedLevel * 255) / 10;
-            turnSpeed = (currentSpeed * 9) / 10;      // 90% of current speed
-            diagonalSpeed = (currentSpeed * 85) / 100; // 85% of current speed
-        }
-        return;
-    }
+// // Function to convert string to integer
+// int stringToInt(char* str) {
+//     int result = 0;
+//     int i = 0;
+//     while (str[i] != '\0') {
+//         result = result * 10 + (str[i] - '0');
+//         i++;
+//     }
+//     return result;
+// }
+
+// void processCommand(char* command) {
+//     if (isNumber(command)) {
+//         int speedLevel = stringToInt(command);
+//         if (speedLevel >= 0 && speedLevel <= 10) {
+//             currentSpeed = (speedLevel * 255) / 10;
+//             turnSpeed = (currentSpeed * 9) / 10;      // 90% of current speed
+//             diagonalSpeed = (currentSpeed * 85) / 100; // 85% of current speed
+//         }
+//         return;
+//     }
     
-    // Movement commands
-    if (command[0] == 'F' && command[1] == '\0') {
-        // Forward
-        forward(currentSpeed);
+//     // Movement commands
+//     if (command[0] == 'F' && command[1] == '\0') {
+//         // Forward
+//         forward(130);
+//         Serial.println("forwards");
         
-    } else if (command[0] == 'B' && command[1] == '\0') {
-        // Backward
-        backwards(currentSpeed);
+//     } else if (command[0] == 'B' && command[1] == '\0') {
+//         // Backward
+//         backwards(currentSpeed);
         
-    } else if (command[0] == 'L' && command[1] == '\0') {
-        // Turn Left
-        turnLeft(turnSpeed);
+//     } else if (command[0] == 'L' && command[1] == '\0') {
+//         // Turn Left
+//         turnLeft(turnSpeed);
         
-    } else if (command[0] == 'R' && command[1] == '\0') {
-        // Turn Right
-        turnRight(turnSpeed);
+//     } else if (command[0] == 'R' && command[1] == '\0') {
+//         // Turn Right
+//         turnRight(turnSpeed);
         
-    } else if (command[0] == 'F' && command[1] == 'L' && command[2] == '\0') {
-        // Forward Left (diagonal)
-        leftMotor(diagonalSpeed / 2);
-        rightMotor(diagonalSpeed);
+//     } else if (command[0] == 'F' && command[1] == 'L' && command[2] == '\0') {
+//         // Forward Left (diagonal)
+//         leftMotor(diagonalSpeed / 2);
+//         rightMotor(diagonalSpeed);
         
-    } else if (command[0] == 'F' && command[1] == 'R' && command[2] == '\0') {
-        // Forward Right (diagonal)
-        leftMotor(diagonalSpeed);
-        rightMotor(diagonalSpeed / 2);
+//     } else if (command[0] == 'F' && command[1] == 'R' && command[2] == '\0') {
+//         // Forward Right (diagonal)
+//         leftMotor(diagonalSpeed);
+//         rightMotor(diagonalSpeed / 2);
         
-    } else if (command[0] == 'B' && command[1] == 'L' && command[2] == '\0') {
-        // Backward Left (diagonal)
-        leftMotor(-diagonalSpeed / 2);
-        rightMotor(-diagonalSpeed);
+//     } else if (command[0] == 'B' && command[1] == 'L' && command[2] == '\0') {
+//         // Backward Left (diagonal)
+//         leftMotor(-diagonalSpeed / 2);
+//         rightMotor(-diagonalSpeed);
         
-    } else if (command[0] == 'B' && command[1] == 'R' && command[2] == '\0') {
-        // Backward Right (diagonal)
-        leftMotor(-diagonalSpeed);
-        rightMotor(-diagonalSpeed / 2);
+//     } else if (command[0] == 'B' && command[1] == 'R' && command[2] == '\0') {
+//         // Backward Right (diagonal)
+//         leftMotor(-diagonalSpeed);
+//         rightMotor(-diagonalSpeed / 2);
         
-    } else if (command[0] == 'S' && command[1] == '\0') {
-        // Stop
-        emergencyStop();
-    }
-}
+//     } else if (command[0] == 'S' && command[1] == '\0') {
+//         // Stop
+//         emergencyStop();
+//     }
+// }
 
-void bluetoothUpdate(void) {
-    int available;
-    char inChar;
+// void bluetoothUpdate(void) {
+//     int available;
+//     char inChar;
     
-    available = Serial.available();
+//     available = Serial.available();
     
-    while (available > 0) {
-        inChar = (char)Serial.read();
+//     while (available > 0) {
+//         Serial.println("Data recieved");
+//         inChar = (char)Serial.read();
         
-        if (inChar == '\n' || inChar == '\r') {
-            if (bufferIndex > 0) {
-                commandBuffer[bufferIndex] = '\0';
-                processCommand(commandBuffer);
-                bufferIndex = 0;
-            }
-        } else {
-            if (bufferIndex < 9) {
-                commandBuffer[bufferIndex] = inChar;
-                bufferIndex++;
-            } else {
-                bufferIndex = 0;
-            }
-        }
+//         if (inChar == '\n' || inChar == '\r') {
+//             if (bufferIndex > 0) {
+//                 commandBuffer[bufferIndex] = '\0';
+//                 processCommand(commandBuffer);
+//                 bufferIndex = 0;
+//             }
+//         } else {
+//             if (bufferIndex < 9) {
+//                 commandBuffer[bufferIndex] = inChar;
+//                 bufferIndex++;
+//             } else {
+//                 bufferIndex = 0;
+//             }
+//         }
         
-        available = Serial.available();
-    }
-}
+//         available = Serial.available();
+//     }
+// }
 
-void bluetoothSend(char* message) {
-    Serial.println(message);
-}
+// void bluetoothSend(char* message) {
+//     Serial.println(message);
+// }
 
-int getCurrentSpeed(void) {
-    return currentSpeed;
-}
+// int getCurrentSpeed(void) {
+//     return currentSpeed;
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <pins.h>
 #include <motor.h>
+#include "bluetooth.h"
+#include <SoftwareSerial.h>
+
+unsigned long oldTime;
+unsigned long deltaTime;
+unsigned long updateDelay;
 
 void setup() {
   pinMode(MOTOR_LEFT_P, OUTPUT);
@@ -13,20 +19,31 @@ void setup() {
   pinMode(LED_RIGHT_INSIDE, INPUT);
   pinMode(LED_RIGHT_OUTSIDE, INPUT);
 
-  Serial.begin(9600);
+    bluetoothInit();
 }
 
 void loop() {
-  turnRight(90); // 0-255 max 
-  delay(1000);
-  emergencyStop();
-  delay(1000);
-  turnLeft(90);
-  delay(1000);
-  emergencyStop();
-  delay(1000);
-  forward(90);
-  delay(2000);
-  emergencyStop();
-  delay(5000);
+  unsigned long now = millis();
+  deltaTime = now - oldTime;
+  oldTime = now;
+
+  bluetoothUpdate();
+  
+  // if(updateDelay >= 100){
+  //     // Check for bluetooth
+  //     // Reset the delay timer.
+  //     updateDelay = 0;
+  // }
+  // turnRight(90); // 0-255 max 
+  // delay(1000);
+  // emergencyStop();
+  // delay(1000);
+  // turnLeft(90);
+  // delay(1000);
+  // emergencyStop();
+  // delay(1000);
+  // forward(90);
+  // delay(2000);
+  // emergencyStop();
+  // delay(5000);
 }
