dba6177e459336a0f2c3995e4b8ada2aa9cb0adf
Luckyw0lf <mcluckyw0lf@gmail.com>
2025-11-14 11:21:19 +0100

    fixed pin assignments

--- include/pins.h
+++ include/pins.h
@@ -2,11 +2,11 @@
 #define PINS_H
 
 //motor pins
-#define MOTOR_LEFT_P 10
-#define MOTOR_LEFT_N 9
+#define MOTOR_LEFT_P 5
+#define MOTOR_LEFT_N 6
 
-#define MOTOR_RIGHT_P 6
-#define MOTOR_RIGHT_N 5
+#define MOTOR_RIGHT_P 9
+#define MOTOR_RIGHT_N 10
 
 // ultrasonic pins
 #define ULTRASONIC_TRIGGER 3
