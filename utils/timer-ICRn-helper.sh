2d8aa2c782e10abb44ab62231dcce25e87513cb3
example <example@example.org>
2025-11-26 19:34:06 +0100

    utils/timer-ICRn-helper.sh: include TIMER2 support

--- utils/timer-ICRn-helper.sh
+++ utils/timer-ICRn-helper.sh
@@ -3,11 +3,20 @@
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3
 
-F_CPU="16000000"
-if [ -n "$1" ]; then
+F_CLK="16000000"		#16 MHz
+F_CLK_TIMER2="32768"	#32768 Hz
+
+if [ -n "$1" ] && [ -n "$2" ]; then
+	#select clock frequency
+	if [ "$1" == "timer0" ] || [ "$1" == "timer1" ]; then
+		FREQ=$F_CLK;
+	elif [ "$1" == "timer2" ]; then
+		FREQ=$F_CLK_TIMER2;
+	fi
+
 	# for all prescaler values
 	for i in {1,8,64,256,1024}; do
-		num=$(echo "($1 / (1/$F_CPU * $i) - 1)" | bc -l);
+		num=$(echo "($2 / (1/$FREQ * $i) - 1)" | bc -l);
 		int=$(printf "%.0f" "$num");
 
 		printf "prescaler %4d\tICRn = " "$i";
@@ -22,7 +31,8 @@ if [ -n "$1" ]; then
 		fi
 	done
 else
-	echo "$0 \"(10 ^ -6)\""
+	printf "$0: {timer0|timer1|timer2} *desired timer freq*\n"
+	printf "$0 timer1 \"(10 ^ -6)\"\n"
 fi
 
 
